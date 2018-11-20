14a8b4054d01b346433402783d1bcb5a36b653ed
cdnicoll <cdnicoll@gmail.com>
2018-11-20 15:05:23 -0800

    added ajax call to albumlist to get a list of albums

--- album/src/components/AlbumList.js
+++ album/src/components/AlbumList.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import {
-    View
+    View,
+    Text
 } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends React.Component {
+    state = { albums: [] };
+
+    async componentWillMount() {
+        try {
+            const { data } = await axios.get('https://rallycoding.herokuapp.com/api/music_albums');
+            this.setState({ albums: data });
+        } catch(err) {
+            console.log(err.response.data);
+        }
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album =>
+            <Text key={album.title}>{ album.title }</Text>
+        );
+    }
+
     render() {
         return (
-            <View></View>
+            <View>{ this.renderAlbums() }</View>
         );
     }
 }
