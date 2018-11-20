d12a3343fc1b64ccc6d05e8429ce584f8c3b0465
cdnicoll <cdnicoll@gmail.com>
2018-11-20 14:07:42 -0800

    Created Albumlist component shell

--- album/App.js
+++ album/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
 // components
-import Header from './src/components/header';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 export default class App extends React.Component {
   render() {
     return (
       <View>
         <Header headerText="Albums!" />
+        <AlbumList />
       </View>
     );
   }
