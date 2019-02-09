9e68083f89926673b2fde55b729e723c7aca731c
Cody Nicoll <cdnicoll@gmail.com>
2019-02-09 11:38:52 -0800

    Added common components

--- tech_stack/App.js
+++ tech_stack/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux'
-import reducers from './src/reducers'
+import { createStore } from 'redux';
+import reducers from './src/reducers';
+import { Header } from './src/components/common';
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducers)}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <View>
+          <Header headerText='Tech Stack' />
+        </View>
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
