cf3453703151c86a858d54a8fe634b72d2962cd6
Cody Nicoll <cdnicoll@gmail.com>
2019-02-11 15:38:58 -0800

    ⭐️ added redux action and reducers

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { emailChanged } from '../actions';
 import { Card, CardSection, Button, Input } from './common';
 
 class LoginForm extends React.Component {
+  onEmailChange = text => {
+    this.props.emailChanged(text);
+  };
+
   render() {
     return (
       <Card>
         <CardSection>
-          <Input label='Email' placeholder='email@email.com' />
+          <Input
+            label='Email'
+            placeholder='email@gmail.com'
+            onChangeText={this.onEmailChange.bind(this)}
+            value={this.props.email}
+          />
         </CardSection>
         <CardSection>
           <Input label='Password' placeholder='password' />
@@ -19,4 +30,13 @@ class LoginForm extends React.Component {
   }
 }
 
-export default LoginForm;
+const mapStateToProps = state => {
+  return {
+    email: state.auth.email,
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  {emailChanged},
+)(LoginForm);
