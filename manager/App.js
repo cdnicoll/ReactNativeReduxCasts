755d7619b4926590f4cfae3f897b886178e5b34d
Cody Nicoll <cdnicoll@gmail.com>
2019-02-11 12:09:42 -0800

    Added firebase setup

--- manager/App.js
+++ manager/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase'
 import reducers from './src/reducers';
 
 export default class App extends React.Component {
+  componentWillMount() {
+    var config = {
+      apiKey: "AIzaSyCc1O-ZFlaCV_LjlTGK1Xnm9_g8o_rEP9M",
+      authDomain: "cdnicoll-manager.firebaseapp.com",
+      databaseURL: "https://cdnicoll-manager.firebaseio.com",
+      projectId: "cdnicoll-manager",
+      storageBucket: "cdnicoll-manager.appspot.com",
+      messagingSenderId: "460119301333"
+    };
+    firebase.initializeApp(config);
+  }
   render() {
     return (
       <Provider store={ createStore(reducers) }>
